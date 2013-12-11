fb8368f1905bc339d62b1456b9c65d44a9e911e0
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-12 09:18:28 +1100

    Data class from old StarField, but no "Array" class (just vectors of vectors)

--- Code/StarField2/Data.cpp
+++ Code/StarField2/Data.cpp
@@ -1,12 +1,58 @@
 #include "Data.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+Data Data::instance;
 
 Data::Data()
+:loaded(false)
 {
 
 }
 
 void Data::load(const char* filename)
 {
+	// Load the data
+	fstream fin(filename, ios::in);
+	if(!fin)
+		cerr<<"Error: Can't load "<<filename<<"."<<endl;
+	char hash;
+	fin>>hash;
+	fin>>ni>>nj>>xMin>>xMax>>yMin>>yMax;
+
+	xRange = xMax - xMin;
+	yRange = yMax - yMin;
+	dx = xRange/nj;
+	dy = yRange/ni;
+	dA = dx*dy;
+
+	pixels.assign(ni, vector<double>(nj));
+
+	for(int i=0; i<ni; i++)
+		for(int j=0; j<nj; j++)
+			fin>>pixels[i][j];
+	fin.close();
+	loaded = true;
+
+	// Assign the xc, yc arrays
+	vector<double> x(nj);
+	vector<double> y(ni);
+	for(int j=0; j<nj; j++)
+		x[j] = xMin + (j + 0.5)*dx;
+	for(int i=0; i<ni; i++)
+		y[i] = yMax - (i + 0.5)*dy;
+	xc = pixels;
+	yc = pixels;
+	for(int i=0; i<ni; i++)
+	{
+		for(int j=0; j<nj; j++)
+		{
+			xc[i][j] = x[j];
+			yc[i][j] = y[i];
+		}
+	}
 
 }
 
