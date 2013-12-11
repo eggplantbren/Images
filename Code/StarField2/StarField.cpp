9b7dc5c492ed80e8a773a12df664bd8ecbd055fa
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-12 09:31:16 +1100

    Set prior on star positions based on data extent (not dodgy because that's prior info!)

--- Code/StarField2/StarField.cpp
+++ Code/StarField2/StarField.cpp
@@ -1,6 +1,9 @@
 #include "StarField.h"
+#include "Data.h"
+
 #include "RandomNumberGenerator.h"
 #include "Utils.h"
+
 #include <cmath>
 
 using namespace std;
@@ -8,9 +11,21 @@ using namespace DNest3;
 
 const int StarField::max_num_stars = 200;
 
+// This looks messy because the limits on the star positions
+// are set based on the extent of the data
 StarField::StarField()
-:stars(3, max_num_stars, false, ClassicMassInf(-50., 50., -50., 50., 1E-3, 1E3))
-,psf(0.1, 10.)
+:stars(3, max_num_stars, false,
+	ClassicMassInf( Data::get_instance().get_xMin()
+			- 0.1*Data::get_instance().get_xRange(),
+			Data::get_instance().get_xMax()
+			+ 0.1*Data::get_instance().get_xRange(),
+			Data::get_instance().get_yMin()
+			- 0.1*Data::get_instance().get_yRange(),
+			Data::get_instance().get_yMax()
+			+ 0.1*Data::get_instance().get_yRange(),
+			1E-3, 1E3))
+,psf(0.5*sqrt(Data::get_instance().get_dA()),
+	20.*sqrt(Data::get_instance().get_dA()))
 {
 
 }
