2b416595b79f2350cbdfaa2458ed1d3dc0105e21
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-11 16:24:48 +1100

    two virtual methods

--- Code/PSF.h
+++ Code/PSF.h
@@ -1,6 +1,8 @@
 #ifndef _PSF_
 #define _PSF_
 
+#include <ostream>
+
 /*
 * Define the interface for any model of a PSF or a pixel-convolved PSF
 */
@@ -14,9 +16,10 @@ class PSF
 		PSF() { }
 		virtual ~PSF() { }
 
-		virtual void fromPrior();
+		virtual void fromPrior() = 0;
 		virtual double perturb() = 0;
 		virtual double evaluate(double x, double y) const = 0;
+		virtual void print(std::ostream& out) const = 0;
 };
 
 #endif
