6c89db84f6db9943a53eceaea0a36e68725f93cb
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-12-12 09:24:40 +1100

    Load the data

--- Code/StarField2/main.cpp
+++ Code/StarField2/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include "Start.h"
 #include "StarField.h"
+#include "Data.h"
 
 using namespace std;
 using namespace DNest3;
 
 int main(int argc, char** argv)
 {
-	#ifndef DNest3_No_Boost
-	MTSampler<StarField> sampler = setup_mt<StarField>(argc, argv);
-	#else
-	Sampler<> sampler = setup<StarField>(argc, argv);
-	#endif
+	// Load a data file
+	Data::load_data("Data/100.txt");
 
+	// Setup and run the sampler
+	MTSampler<StarField> sampler = setup_mt<StarField>(argc, argv);
 	sampler.run();
+
 	return 0;
 }
 
